e79a1593e53adcbf0d95292fcf0952d6c414c341
LotuxPunk <clement@vandendaelen.com>
2018-11-19 10:18:43 +0100

    my conventions

--- src/main/java/com/vandendaelen/openbackup/OpenBackup.java
+++ src/main/java/com/vandendaelen/openbackup/OpenBackup.java
@@ -1,5 +1,6 @@
-package com.example.examplemod;
+package com.vandendaelen.openbackup;
 
+import com.vandendaelen.openbackup.utils.Reference;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -7,13 +8,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Reference.MODID, name = Reference.MOD_NAME, version = Reference.VERSION.VERSION)
+public class OpenBackup
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
     private static Logger logger;
 
     @EventHandler
---
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -0,0 +1,19 @@
+package com.vandendaelen.openbackup.utils;
+
+public class Reference {
+    public static final String MODID = "openbackup";
+    public static final String MOD_NAME = "OpenBackup";
+    public static final String DEP = "required-after:forge@[14.23.4.2768,)";
+    public static final String SERVER_PROXY_CLASS = "com.vandendaelen.openbackup.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.vandendaelen.openbackup.proxy.ClientProxy";
+    public static final String UPDATE_JSON = "https://raw.githubusercontent.com/LotuxPunk/OpenBackup/master/update.json";
+
+    public static class VERSION{
+        public static final String MCVERSION = "1.12.2";
+        public static final String MAJORMOD = "0";
+        public static final String MAJORAPI = "0";
+        public static final String MINOR = "0";
+        public static final String PATCH = "1";
+        public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
+    }
+}
