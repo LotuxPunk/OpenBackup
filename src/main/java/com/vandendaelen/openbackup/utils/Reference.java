9396a8d742e9cc43729292d23096f5d882e1f00a
LotuxPunk <clement@vandendaelen.com>
2018-11-22 00:42:00 +0100

    version bump

--- src/main/java/com/vandendaelen/openbackup/utils/Reference.java
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -13,7 +13,7 @@ public class Reference {
         public static final String MAJORMOD = "0";
         public static final String MAJORAPI = "0";
         public static final String MINOR = "0";
-        public static final String PATCH = "1";
+        public static final String PATCH = "2";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
 }
