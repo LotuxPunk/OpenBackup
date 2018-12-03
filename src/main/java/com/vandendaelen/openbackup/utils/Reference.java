556be08aa946c07341e0d1e88565ede0e55ae896
LotuxPunk <clement@vandendaelen.com>
2018-12-03 10:37:31 +0100

    patch

--- src/main/java/com/vandendaelen/openbackup/utils/Reference.java
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -12,7 +12,7 @@ public class Reference {
         public static final String MCVERSION = "1.12.2";
         public static final String MAJORMOD = "0";
         public static final String MAJORAPI = "0";
-        public static final String MINOR = "0";
+        public static final String MINOR = "1";
         public static final String PATCH = "7";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
