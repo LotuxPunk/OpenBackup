3ea54b0bdbc523c3be8ebd97cf39b611daf65c75
LotuxPunk <clement@vandendaelen.com>
2019-07-10 10:54:11 +0200

    oops

--- src/main/java/com/vandendaelen/openbackup/utils/Reference.java
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -3,15 +3,15 @@ package com.vandendaelen.openbackup.utils;
 public class Reference {
     public static final String MODID = "openbackup";
     public static final String MOD_NAME = "OpenBackup";
-    public static final String DEP = "required-after:forge@[25.0.17,)";
+    public static final String DEP = "required-after:forge@[27.0.47,)";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/LotuxPunk/OpenBackup/master/update.json";
 
     public static class VERSION{
-        public static final String MCVERSION = "1.13.2";
+        public static final String MCVERSION = "1.14.3";
         public static final String MAJORMOD = "0";
         public static final String MAJORAPI = "0";
         public static final String MINOR = "1";
-        public static final String PATCH = "1";
+        public static final String PATCH = "2";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
 }
