fcf0d9a612df74115dbd74234ffc460dfddd2a56
LotuxPunk <vandendaelenclement@gmail.com>
2019-09-15 16:47:10 +0200

    port to Recommended build

--- src/main/java/com/vandendaelen/openbackup/utils/Reference.java
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -11,7 +11,7 @@ public class Reference {
         public static final String MAJORMOD = "0";
         public static final String MAJORAPI = "0";
         public static final String MINOR = "1";
-        public static final String PATCH = "2";
+        public static final String PATCH = "4";
         public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
     }
 }
