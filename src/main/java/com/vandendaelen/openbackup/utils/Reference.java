e012f226aa2cd3482839d464ab86c4741e21031a
LotuxPunk <vandendaelenclement@gmail.com>
2020-06-21 11:20:54 +0200

    update forge version

--- src/main/java/com/vandendaelen/openbackup/utils/Reference.java
+++ src/main/java/com/vandendaelen/openbackup/utils/Reference.java
@@ -5,13 +5,4 @@ public class Reference {
     public static final String MOD_NAME = "OpenBackup";
     public static final String DEP = "required-after:forge@[27.0.47,)";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/LotuxPunk/OpenBackup/master/update.json";
-
-    public static class VERSION{
-        public static final String MCVERSION = "1.14.4";
-        public static final String MAJORMOD = "0";
-        public static final String MAJORAPI = "0";
-        public static final String MINOR = "1";
-        public static final String PATCH = "4";
-        public static final String VERSION = MCVERSION+"-"+MAJORMOD+"."+MAJORAPI+"."+MINOR+"."+PATCH;
-    }
 }
