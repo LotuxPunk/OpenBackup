6471fdaa14b9f90be11d7926be9e8536f358a27a
LotuxPunk <vandendaelenclement@gmail.com>
2020-08-19 12:46:59 +0200

    First commit

---
+++ openbackup-fabric/src/main/java/com/vandendaelen/openbackup/OpenbackupFabric.java
@@ -0,0 +1,12 @@
+package com.vandendaelen.openbackup;
+
+public class OpenbackupFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        System.out.println("Hello Fabric world!");
+    }
+}
